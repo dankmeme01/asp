e657c64933db98863e262c392a5e23addfacf396
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2024-04-05 08:41:11 +0200

    mm macos

--- include/asp/detail/Detail.hpp
+++ include/asp/detail/Detail.hpp
@@ -2,26 +2,26 @@
 
 #include <bit>
 
-namespace asp::detail {
-
 #if defined(__cpp_lib_bit_cast) && __cpp_lib_bit_cast >= 201806L
-template <typename To, typename From>
-static inline constexpr To bit_cast(From value) noexcept {
-    return std::bit_cast<To, From>(value);
+namespace asp::detail {
+    template <typename To, typename From>
+    static inline constexpr To bit_cast(From value) noexcept {
+        return std::bit_cast<To, From>(value);
+    }
 }
 #else
 #include <type_traits>
 #include <cstring>
-template <typename To, typename From>
-static inline
-std::enable_if_t<sizeof(To) == sizeof(From) && std::is_trivially_copyable_v<From> && std::is_trivially_copyable_v<To>, To>
-bit_cast(From value) noexcept {
-    static_assert(std::is_trivially_constructible_v<To>, "destination template argument for bit_cast must be trivially constructible");
+namespace asp::detail {
+    template <typename To, typename From>
+    static inline
+    ::std::enable_if_t<sizeof(To) == sizeof(From) && ::std::is_trivially_copyable_v<From> && std::is_trivially_copyable_v<To>, To>
+    bit_cast(From value) noexcept {
+        static_assert(std::is_trivially_constructible_v<To>, "destination template argument for bit_cast must be trivially constructible");
 
-    To dest;
-    std::memcpy(&dest, &value, sizeof(To));
-    return dest;
+        To dest;
+        ::std::memcpy(&dest, &value, sizeof(To));
+        return dest;
+    }
 }
 #endif
-
-}
