841980a76b00b4e3680e0808df17494a0d6bfe6d
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2024-04-05 08:34:33 +0200

    yay

--- include/asp/thread/Thread.hpp
+++ include/asp/thread/Thread.hpp
@@ -7,6 +7,7 @@
 #include <functional>
 #include <memory>
 #include <thread>
+#include <stdexcept>
 
 namespace asp::thread {
 
--- src/assert.cpp
+++ src/assert.cpp
@@ -1,5 +1,6 @@
 #include <asp/config.hpp>
 #include <stdexcept>
+#include <string>
 
 void asp::detail::assertionFail(const char* message) {
     throw std::runtime_error(std::string("asp assertion failed: ") + message);
