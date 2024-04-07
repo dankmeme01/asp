46e7c584135341561211ed3fc1ffdbb8e4ee45da
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2024-04-07 23:31:42 +0200

    add async macros

--- include/asp/async/Macros.hpp
+++ include/asp/async/Macros.hpp
@@ -1,1 +1,19 @@
 #pragma once
+
+// macro allowing for syntax like $into_async(socket.send(data)).await();
+#define $into_async(expr) ::asp::async::spawn([&] { return expr; })
+
+// same as `$into_async` but captures variables by copy instead of reference
+#define $into_async_copy(expr) ::asp::async::spawn([=] { return expr; })
+
+// macro allowing for syntax like this:
+//
+// ```
+// auto asyncFunc(int x) $async ({
+//     // your code
+//     return 3;
+// })
+// ```
+//
+// the invokation of `asyncFunc` will not run the code inside immediately, and instead return a `FutureHandle`.
+#define $async(block) { return ::asp::async::spawn([] { block }); }
